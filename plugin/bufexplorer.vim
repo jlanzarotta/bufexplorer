jlanzarotta/bufexplorer

commit 4f20377909acd781ebe9d8a46d4d5d7e370d7475

Reset splitMode
If you use either split mode, you can no longer use the regular non-split mode. This is because the split mode set s:splitMode and that variable is never reset to "" to allow you run without split mode.
This change simply resets s:splitMode to "" after completing the split mode bufexplorer.

plugin/bufexplorer.vim
@@ -392,12 +392,14 @@ endfunction
 function! BufExplorerHorizontalSplit()
     let s:splitMode = "sp"
     execute "BufExplorer"
+    let s:splitMode = ""
 endfunction
 
 " BufExplorerVerticalSplit {{{2
 function! BufExplorerVerticalSplit()
     let s:splitMode = "vsp"
     execute "BufExplorer"
+    let s:splitMode = ""
 endfunction
 
 " ToggleBufExplorer {{{2

commit f90f4465693b002d56cf838cc8f67fe890e368d6

Bugfix closing bufexplorer window on select
This fixes a regression introduced by commit 4f20377 (Reset spitMode)

plugin/bufexplorer.vim
@@ -451,6 +451,9 @@ function! BufExplorer()
             execute 'keepalt ' . _size . s:splitMode
         endif
 
+        " Note that the current buffer resulted from a split
+        let b:didSplit = 1
+
         " Restore the original settings.
         let [&splitbelow, &splitright] = [_splitbelow, _splitright]
     endif
@@ -1038,7 +1041,7 @@ function! s:Close()
     endif
 
     " If we needed to split the main window, close the split one.
-    if s:splitMode != "" && bufwinnr(s:originBuffer) != -1
+    if exists("b:didSplit") && b:didSplit == 1 && bufwinnr(s:originBuffer) != -1
         execute "wincmd c"
     endif
 