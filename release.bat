f1b5001f0cac2cc0ac7f9f634cfea5cb8b92ba84
Jeff Lanzarotta <jlanzarotta@matw.com>
2024-11-07 08:56:37 -0500

    Added simple release batchfile.

---
+++ release.bat
@@ -0,0 +1,19 @@
+@echo off
+
+if [%1]==[] goto :help
+
+setlocal enableDelayedExpansion
+set /p "GH_TOKEN=" <GITHUB_TOKEN
+
+git tag -a %1 -m "Release %1."
+git push origin %1
+
+gh release %1 --notes-from-tag
+
+goto :done
+
+:help
+echo Usage %0 version_number
+goto :done
+
+:done
